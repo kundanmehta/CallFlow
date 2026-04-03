ebd3a7dfae1e09fe14c2720234a0a56b87e00466
Kundan Mehta <kundanmehta2501@gmail.com>
2026-04-03 13:16:01 +0530

    config file edited

--- config/db.php
+++ config/db.php
@@ -10,13 +10,12 @@ if ($isLocal) {
     $username = 'root';
     $password = '';
     $database = 'lead';
-}
-else {
+} else {
     // ── LIVE SERVER ────────────────────────────
     $host = 'localhost';
-    $username = 'u823573651_lead';
-    $password = 'l|93mj3S:';
-    $database = 'u823573651_lead';
+    $username = 'u523255408_lead';
+    $password = '>obQk!N7';
+    $database = 'u523255408_lead';
 }
 
 try {
@@ -24,10 +23,7 @@ try {
     // Set PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-}
-catch (PDOException $e) {
+} catch (PDOException $e) {
     die("Database Connection Failed: " . $e->getMessage());
 }
 ?>
-
-
